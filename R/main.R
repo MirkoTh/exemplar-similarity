9a20bba364df2c89613506d570dc06da59ec2f03
MirkoTh <mirkothalmann@hotmail.com>
2022-01-11 16:43:27 +0100

    sample from cluster centers controlling for cluster distances

--- R/main.R
+++ R/main.R
@@ -12,28 +12,51 @@ library(MASS)
 # 5. manually play around with the other parameters to achieve reasonable predictions
 
 
-xy_space <- function(x_range, seed,  x_nudge = 1, fn = "sin"){
+xy_space <- function(fn, smoothness, x_range, seed, x_nudge = 1){
   if (sum(x_range < 0) != 0 | !is.integer(x_range[1]) | !is.integer(x_range[2])) {
     stop("only positive integers allowed in x range")
   } 
   x <- seq(x_range[1] + x_nudge, x_range[2] - x_nudge, by = 1)
   tbl_x <- crossing(x1 = x, x2 = x)
+  tbl_x$y <- fn(tbl_x, seed, smoothness)
+  tbl_x$smoothness <- smoothness
+  return(tbl_x)
+}
+
+fn_sin <- function(tbl, seed, smoothness) {
+  if (smoothness == "smooth") {
+    scale <- 3
+  } else if (smoothness == "rough") {
+    scale <- 1
+  }
   set.seed(seed)
   shift_x <- runif(2, 0, 2*pi)
-  if (fn == "sin") {
-    tbl_x$y <- sin(tbl_x$x1 + shift_x[1]) + sin(tbl_x$x2 + shift_x[2])
-  }
-  return(tbl_x)
+  return(sin(tbl$x1 + shift_x[1]) / scale + sin(tbl$x2 + shift_x[2]) / scale)
 }
 
 xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded) {
   x_range_constrained <- c(min(tbl_xy$x1), max(tbl_xy$x2))
-  x_centers <- sample(seq(x_range_constrained[1], x_range_constrained[2], by = 1), n_centers*2, replace = FALSE)
-  groups <- rep(seq(1, length(x_centers)/2, by = 1), each = 2)
-  l_centers <- split(x_centers, groups)
+  
+  # make distance between centers large enough
+  min_distance <- 0
+  while(min_distance > 5){
+    x_centers <- sample(
+      seq(x_range_constrained[1], x_range_constrained[2], by = 1), n_centers*2, replace = FALSE
+    )
+    groups <- rep(seq(1, length(x_centers)/2, by = 1), each = 2)
+    n_groups <- max(groups)
+    l_centers <- split(x_centers, groups)
+    dist_eucl <- function(a, b, l) sqrt((l[[a]][1] - l[[b]][1])^2 + (l[[a]][2] - l[[b]][2])^2)
+    cross_centers <- crossing(a = 1:length(l_centers), b = 1:length(l_centers))
+    min_distance <- min(unlist(pmap(cross_centers, dist_eucl, l_centers)))
+  }
   n_crowded <- round(n_train * p_crowded)
+  unevens <- n_crowded %% n_groups
+  n_crowded <- n_crowded + (n_groups - unevens)
   n_sparse <- n_train - n_crowded
-  x_crowded <- mvrnorm(n_crowded, l_centers[[1]], matrix(c(1, 0, 0, 1), ncol = 2))
+  x_crowded <- map(
+    l_centers, mvrnorm, n = n_crowded / n_groups, Sigma = matrix(c(1, 0, 0, 1), ncol = 2)
+  ) %>% reduce(rbind)
   x_sparse <- cbind(
     runif(n_sparse, x_range_constrained[1], x_range_constrained[2]),
     runif(n_sparse, x_range_constrained[1], x_range_constrained[2])
@@ -41,21 +64,58 @@ xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded) {
   m_xy_train <- rbind(x_crowded, x_sparse)
   colnames(m_xy_train) <- c("x1", "x2")
   tbl_xy_train <- as_tibble(m_xy_train)
+  tbl_xy_train$crowding <- factor(c(rep("crowded", n_crowded), rep("sparse", n_sparse)))
   return(tbl_xy_train)
 }
 
+
+plot_xy <- function(tbl) {
+  ggplot(tbl, aes(x1, x2)) +
+    geom_tile(aes(fill = y)) +
+    geom_text(aes(label = round(y, 1)), color = "white") +
+    scale_fill_viridis_c() +
+    theme_bw() +
+    labs(
+      x = expression(X["1"]),
+      y = expression(X["2"])
+    )
+}
+
+plot_x_train <- function(tbl) {
+  ggplot(tbl, aes(x1, x2, group = crowding)) +
+    geom_point(aes(color = crowding)) +
+    scale_color_brewer(palette = "Set1") +
+    theme_bw() +
+    labs(
+      x = expression(X["1"]),
+      y = expression(X["2"])
+    )
+}
+
 x_range <- c(0L, 15L)
-n <- 
-tbl_xy <- xy_space(x_range, 2)
-tbl_xy_train <- xy_sample(tbl_xy, 100, 3, .8)
+n_train <- 80
+n_centers <- 3
+n_smoothness <- 2
+p_crowded <- .8
+l_info <- list(
+  "x_range" = x_range, "n_train" = n_train,
+  "n_center" = n_centers, "n_smoothness" = n_smoothness,
+  "p_crowded" = p_crowded
+)
 
+space_and_exemplars <- function(smoothness, seed, l_info, fn) {
+  tbl_xy <- xy_space(fn, smoothness, l_info[["x_range"]], seed)
+  tbl_xy_train <- xy_sample(tbl_xy, l_info[["n_train"]], l_info[["n_center"]], l_info[["p_crowded"]])
+  tbl_xy_train$y <- fn(tbl_xy_train, 2, smoothness)
+  tbl_xy_train$smoothness <- smoothness
+  return(list(tbl_xy, tbl_xy_train))
+}
 
-
-ggplot(tbl_xy, aes(x1, x2)) +
-  geom_tile(aes(fill = y)) +
-  geom_text(aes(label = round(y, 1)), color = "white") +
-  scale_fill_viridis_c() +
-  theme_bw()
+l_smooth <- map(c("smooth", "rough"), space_and_exemplars, seed = 1423, l_info = l_info, fn = fn_sin)
+tbl_space <- reduce(map(l_smooth, 1), rbind)
+tbl_xy_train <- reduce(map(l_smooth, 2), rbind)
+plot_xy(tbl_space) + facet_wrap(~ smoothness)
+plot_x_train(tbl_xy_train) + facet_wrap(~ smoothness)
 
 
 
