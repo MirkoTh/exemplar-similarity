9de7a29e64ab1c329ff4b3a1aae33e87756df4ab
Mirko Thalmann <mirkothalmann@hotmail.com>
2022-01-12 19:16:54 +0100

    add kernel functions and sample test data in pipeline

--- R/main.R
+++ R/main.R
@@ -1,3 +1,6 @@
+
+# Load Packages and Home-Grown Functions ----------------------------------
+
 library(tidyverse)
 library(docstring)
 library(MASS)
@@ -10,41 +13,96 @@ walk(l, source)
 # CHECK 1. draw samples from the rough and the smooth functions
 # CHECK 1.1 create regions with high density and low density of training points
 # 
-# 2. fit the gp model in both conditions
+# CHECK fit the gp model in both conditions
 # 3. evaluate the kernel function for the intended test points
 # 4. feed the summed similarity measure into the linear ba model
 # 5. manually play around with the other parameters to achieve reasonable predictions
 
 
 
-# simulation parameters
+
+# Define Simulation Parameters --------------------------------------------
+
+
 x_range <- c(0L, 15L)
-n_train <- 80
+n_train <- 100
+n_test <- 100
 n_centers <- 4
 n_smoothness <- 2
-p_crowded <- .8
+p_crowded_train <- .8
+p_crowded_test <- .5
+smooth_vals <- c("rough", "smooth")
 l_info <- list(
-  "x_range" = x_range, "n_train" = n_train,
+  "x_range" = x_range, 
+  "n_train" = n_train, "n_test" = n_test,
   "n_center" = n_centers, "n_smoothness" = n_smoothness,
-  "p_crowded" = p_crowded
+  "p_crowded_train" = p_crowded_train,
+  "p_crowded_test" = p_crowded_test
 )
+
+
+
+# Simulate Data From Conditions -------------------------------------------
+
 
 # draw samples from smooth and rough conditions
 # there are x regions with high and with low density (crowded vs. sparse)
+# p crowded differs between training and test
 
-l_smooth <- map(c("smooth", "rough"), space_and_exemplars, seed = 7323, l_info = l_info, fn = fn_sin)
+l_smooth <- map(smooth_vals, space_and_exemplars, seed = 7323, l_info = l_info, fn = fn_sin)
 tbl_space <- reduce(map(l_smooth, 1), rbind)
+
 tbl_xy_train <- reduce(map(l_smooth, 2), rbind)
 tbl_xy_train$smoothness <- as.factor(tbl_xy_train$smoothness)
 l_tbl_xy_train <- split(tbl_xy_train, ~ smoothness)
 
+tbl_xy_test <- reduce(map(l_smooth, 3), rbind)
+tbl_xy_test$smoothness <- as.factor(tbl_xy_test$smoothness)
+l_tbl_xy_test <- split(tbl_xy_test, ~ smoothness)
+
 # overview plots
 plot_xy(tbl_space) + facet_wrap(~ smoothness)
 plot_x_train(tbl_xy_train) + facet_wrap(~ smoothness)
+plot_x_train(tbl_xy_test) + facet_wrap(~ smoothness)
+
+# save experimental data here that can be loaded from within python
+write_json(l_tbl_xy_train, "data/l-data-train.json")
 
 
-tbl_xy_train
+
+# Execute Model Fitting in Python -----------------------------------------
+
+
+shell.exec("python\\run-python.bat")
+Sys.sleep(5)
 
 
 
+# Import Fitted Parameters from Python ------------------------------------
 
+
+l_params_fitted <- read_json("data/model-params.json")
+names(l_params_fitted) <- smooth_vals
+
+
+x_1 <- seq(1, 12)
+x_2 <- x_1
+
+tbl_x <- crossing(
+  x_1, x_2
+)
+
+N <- nrow(tbl_x)
+
+tbl_sim_rbf <- similarity_rbf(2, 1, tbl_x, N)
+
+cols <- colnames(tbl_sim_rbf)
+tbl_sim_rbf$var2 <- cols
+
+
+tbl_sim_rbf %>% pivot_longer(cols = all_of(cols)) %>%
+  mutate(var2 = factor(var2, levels = cols),
+         name = factor(name, levels = cols)) %>%
+  ggplot() +
+  geom_tile(aes(name, var2, color=value)) +
+  scale_color_viridis_c()
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 xy_space <- function(fn, smoothness, x_range, seed, x_nudge = 1){
   #' @description create xy space only using integers as a grid
   #' @param smoothness stating whether a "smooth" or "rough" function should be sampled from
@@ -32,7 +34,7 @@ fn_sin <- function(tbl, seed, smoothness) {
   return(sin(tbl$x1 + shift_x[1]) / scale + sin(tbl$x2 + shift_x[2]) / scale)
 }
 
-xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded) {
+xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded, l_centers = NULL) {
   #' @description sampling x values and respective y values
   #' a proportion of x values (p_crowded) are sampled from n cluster centers (n_centers)
   #' 
@@ -45,20 +47,21 @@ xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded) {
   x_range_constrained <- c(min(tbl_xy$x1), max(tbl_xy$x2))
   # make distance between centers large enough
   min_distance <- 0
-  while(min_distance < 5){
-    x_centers <- sample(
-      seq(x_range_constrained[1], x_range_constrained[2], by = 1), n_centers*2, replace = FALSE
-    )
-    grps <- rep(seq(1, length(x_centers)/2, by = 1), each = 2)
-    n_grps <- max(grps)
-    l_centers <- split(x_centers, grps)
-    dist_eucl <- function(a, b, l) sqrt((l[[a]][1] - l[[b]][1])^2 + (l[[a]][2] - l[[b]][2])^2)
-    cross_centers <- crossing(a = 1:length(l_centers), b = 1:length(l_centers))
-    distances <- unlist(pmap(cross_centers, dist_eucl, l_centers))
-    distances <- distances[distances != 0]
-    min_distance <- min(distances)
-  }
-  print(l_centers)
+  if (is.null(l_centers)){
+    while(min_distance < 5){
+      x_centers <- sample(
+        seq(x_range_constrained[1], x_range_constrained[2], by = 1), n_centers*2, replace = FALSE
+      )
+      grps <- rep(seq(1, length(x_centers)/2, by = 1), each = 2)
+      n_grps <- max(grps)
+      l_centers <- split(x_centers, grps)
+      dist_eucl <- function(a, b, l) sqrt((l[[a]][1] - l[[b]][1])^2 + (l[[a]][2] - l[[b]][2])^2)
+      cross_centers <- crossing(a = 1:length(l_centers), b = 1:length(l_centers))
+      distances <- unlist(pmap(cross_centers, dist_eucl, l_centers))
+      distances <- distances[distances != 0]
+      min_distance <- min(distances)
+    }
+  } else {n_grps <- length(l_centers)}
   n_crowded <- round(n_train * p_crowded)
   unevens <- n_crowded %% n_grps
   n_crowded <- n_crowded + (n_grps - unevens)
@@ -74,19 +77,47 @@ xy_sample <- function(tbl_xy, n_train, n_centers, p_crowded) {
   colnames(m_xy_train) <- c("x1", "x2")
   tbl_xy_train <- as_tibble(m_xy_train)
   tbl_xy_train$crowding <- factor(c(rep("crowded", n_crowded), rep("sparse", n_sparse)))
-  return(tbl_xy_train)
+  l_out <- list(tbl_xy_train, l_centers)
+  return(l_out)
 }
 
 
 space_and_exemplars <- function(smoothness, seed, l_info, fn) {
-  #' @description pipeline function to sample for one smoothness condition
+  #' @description pipeline function to sample train and test data for one smoothness condition
   #' @param seed controlling the sampling
   #' @param l_info list with simulation parameters
-  #' @return list with tbl spanning xy space and tbl with sampled values
+  #' @return list with tbl spanning xy space and train & test tbls with sampled values
   #'
   tbl_xy <- xy_space(fn, smoothness, l_info[["x_range"]], seed)
-  tbl_xy_train <- xy_sample(tbl_xy, l_info[["n_train"]], l_info[["n_center"]], l_info[["p_crowded"]])
+  l_train <- xy_sample(tbl_xy, l_info[["n_train"]], l_info[["n_center"]], l_info[["p_crowded_train"]])
+  tbl_xy_train <- l_train[[1]]
+  l_centers <- l_train[[2]]
   tbl_xy_train$y <- fn(tbl_xy_train, 2, smoothness)
   tbl_xy_train$smoothness <- smoothness
-  return(list(tbl_xy, tbl_xy_train))
+  print(l_centers)
+  l_test <- xy_sample(tbl_xy, l_info[["n_test"]], l_info[["n_center"]], l_info[["p_crowded_test"]], l_centers)
+  tbl_xy_test <- l_test[[1]]
+  tbl_xy_test$y <- fn(tbl_xy_test, 2, smoothness)
+  tbl_xy_test$smoothness <- smoothness
+  return(list(tbl_xy, tbl_xy_train, tbl_xy_test))
+}
+
+
+distance_rbf <- function(i, df_x, N, lambda, sigma){
+  m <- matrix(unlist(rep(df_x[i, ], N)), N, 2, byrow = TRUE)
+  colnames(m) <- c("x_1", "x_2")
+  tbl_single <- as_tibble(m)
+  tbl_single["sim"] <- - (sqrt(
+    (tbl_single$x_1 - tbl_x$x_1)^2 +
+      (tbl_single$x_2 - tbl_x$x_2)^2
+  )^2) / (2*lambda^2)
+  return (tbl_single$sim)
+}
+
+similarity_rbf <- function(lambda, sigma, tbl_x, N){
+  l <- map(1:N, distance_rbf, tbl_x, N, lambda, sigma)
+  m <- matrix(unlist(l), N, N, byrow = TRUE)
+  colnames(m) <- str_c("x_", 1:N)
+  tbl <- as_tibble(sigma^2 * exp(m))
+  return (tbl)
 }
